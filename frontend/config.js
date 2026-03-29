89d0f05c0058bced1d0f2f92dfedde21ea8dd5bc
Shahriar Alom Masud <shahriar0002@std.uftb.ac.bd>
2026-03-30 00:02:50 +0600

    Fix model compatibility issue

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
 const API_BASE_URL = "https://oncoscan-ai.onrender.com";
-=======
-const API_BASE_URL = "https://oncoscan-ai.onrender.com";
->>>>>>> 06133f798bb475f220a66a26c173a7d01e3db96e
+
+export default API_BASE_URL;
